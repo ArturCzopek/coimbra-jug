f8c6df05261e25d0552ec96acccbfd5a057b57b0
ArturC <artur.m.czopek@metrosoft.com>
2018-02-20 21:25:25 +0000

    11. CONTROLLER - java, no typealias, @JvmStatic

--- src/main/kotlin/pl/simplecoding/coimbrajug/TokenType.kt
+++ src/main/kotlin/pl/simplecoding/coimbrajug/TokenType.kt
@@ -1,10 +1,16 @@
 package pl.simplecoding.coimbrajug
 
+/**
+ * @Author Artur Czopek
+ * @Link https://simplecoding.pl/coimbra-jug
+ */
+
 enum class TokenType {
     GREEN,
     RED;
 
     companion object {
+        @JvmStatic
         fun fromString(type: String) = when (type) {
             "green" -> GREEN
             "red" -> RED
---
+++ src/main/kotlin/pl/simplecoding/coimbrajug/UserController.java
@@ -0,0 +1,39 @@
+package pl.simplecoding.coimbrajug;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author Artur Czopek
+ * @Link https://simplecoding.pl/coimbra-jug
+ */
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/usersList")
+    public List<Map<String, Object>> getUsers(Principal principal) {
+        return userService.getAllUsers(principal);
+    }
+
+    @GetMapping("/me")
+    public User getData(Principal principal) {
+        return userService.getData(principal);
+    }
+
+    @PostMapping("/action")
+    public void giveToken(@RequestParam long toUserId, @RequestParam String tokenType, Principal principal) {
+        userService.giveToken(toUserId, TokenType.fromString(tokenType), principal);
+    }
+}
