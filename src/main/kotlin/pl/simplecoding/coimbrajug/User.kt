4b440eddbc7ca3fb36f5cb1a1fac9f8c3f5023a5
ArturC <artur.m.czopek@metrosoft.com>
2018-02-20 21:09:31 +0000

    07. USER_INSERT_RUNNER - vararg, named params, foreach (1..20), string $

--- src/main/kotlin/pl/simplecoding/coimbrajug/User.kt
+++ src/main/kotlin/pl/simplecoding/coimbrajug/User.kt
@@ -1,11 +1,15 @@
 package pl.simplecoding.coimbrajug
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.boot.CommandLineRunner
 import org.springframework.data.repository.CrudRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
+
 
 /**
  * @Author Artur Czopek
@@ -32,4 +36,27 @@ interface UserRepository : CrudRepository<User, Long> {
     override fun findAll(): List<User>
 
     fun findOneByName(name: String): User
+}
+
+
+@Service
+class UserInsertRunner(
+        private val userRepository: UserRepository,
+        private val authRepository: AuthRepository,
+        private val bCryptPasswordEncoder: BCryptPasswordEncoder
+) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+
+        (1..20).forEach {
+            val id = it.toLong()
+            val name = "user$id"
+
+            val auth = Authority(id, name, "user")
+            authRepository.save(auth)
+
+            val user = User(name = name, id = id, authorityId = id, password = bCryptPasswordEncoder.encode(name))
+            userRepository.save(user)
+        }
+    }
 }
